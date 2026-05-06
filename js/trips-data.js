2cc818b10e22112ac88f8a34ed109bc04625c44c
Sofiast96 <st9694336@stud.duikt.edu.ua>
2026-05-06 08:27:11 +0300

    trips-data.js

---
+++ js/trips-data.js
@@ -0,0 +1,45 @@
+const featuredTrips = [
+    {
+        id: 1,
+        title: "Centipede Tour - Guided Arizona. Desert Tour by ATV",
+        location: "Paris, France",
+        duration: 4,
+        price: 189.25,
+        rating: 4.8,
+        reviewsCount: 243,
+        imageUrl: "images/Centipede Tour.png"
+    },
+    {
+        id: 2,
+        title: "Molokini and Turtle Town Snorkeling Adventure Aboard",
+        location: "New York, USA",
+        duration: 4,
+        price: 225.00,
+        rating: 4.8,
+        reviewsCount: 243,
+        imageUrl: "images/Molokini.png"
+    },
+    {
+        id: 3,
+        title: "Westminster Walking Tour & Westminster Abbey Entry",
+        location: "London, UK",
+        duration: 4,
+        price: 943.00,
+        rating: 4.8,
+        reviewsCount: 243,
+        imageUrl: "images/Westminster.png"
+    },
+    {
+        id: 4,
+        title: "All Inclusive Ultimate Circle Island Day Tour with Lunch",
+        location: "New York, USA",
+        duration: 4,
+        price: 771.00,
+        rating: 4.8,
+        reviewsCount: 243,
+        imageUrl: "images/All Inclusive Ultimate Circle Island.png"
+    }
+];
+
+console.log("Дані про тури:", featuredTrips);
+export { featuredTrips };
